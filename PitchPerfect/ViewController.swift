2af67568fb7ad67f5f3c250cbfa0c40fb4d816ad
Young-Hwan Mun <mcodegeeks@gmail.com>
2016-06-21 18:19:32 -0400

    Added ViewController and Multiple Views

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecodingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        print("viewWillAppear called")
+        stopRecodingButton.enabled = false
+    }
+    
     @IBAction func recordAudio(sender: AnyObject) {
         print("record button pressed")
         recordingLabel.text = "Recording in progress"
+        recordButton.enabled = false
+        stopRecodingButton.enabled = true
     }
     
     @IBAction func stopRecoding(sender: AnyObject) {
         print("stop recording button pressed")
+        recordingLabel.text = "Tap to Record"
+        recordButton.enabled = true
+        stopRecodingButton.enabled = false
     }
 }
 
